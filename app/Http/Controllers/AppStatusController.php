ce6e52370a7151bb18c64871bf421017d29b00b3
Emmanuel <emmanuel@nucleodev.com>
2020-08-20 23:30:09 -0500

    Optimización de código
    ·Se eliminan líneas innecesarias

--- app/Http/Controllers/AppStatusController.php
+++ app/Http/Controllers/AppStatusController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 
 class AppStatusController extends Controller
 {
-
     const MODEL = 'App\AppStatus';
     use RestActions;
 
@@ -31,79 +30,4 @@ class AppStatusController extends Controller
         return $this->respond('done', $model);
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\AppStatus  $appStatus
-     * @return \Illuminate\Http\Response
-     */
-    public function show(AppStatus $appStatus)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\AppStatus  $appStatus
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(AppStatus $appStatus)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\AppStatus  $appStatus
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, AppStatus $appStatus)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\AppStatus  $appStatus
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(AppStatus $appStatus)
-    {
-        //
-    }
 }
