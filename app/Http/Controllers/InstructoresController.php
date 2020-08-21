0b623814b09c7510b18246a2a953d91f9eaff7ab
Emmanuel <emmanuel@nucleodev.com>
2020-08-20 23:26:05 -0500

    Se crea controlador de instructores

---
+++ app/Http/Controllers/InstructoresController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Instructores;
+use Illuminate\Http\Request;
+
+class InstructoresController extends Controller
+{
+    const MODEL = 'App\Instructores';
+    use RestActions;
+}
