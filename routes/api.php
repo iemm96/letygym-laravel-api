10477aa1508f4e1dcecaa7d464aa5d86c5e7cf71
Emmanuel <emmanuel@nucleodev.com>
2020-08-20 23:28:12 -0500

    Se actualizan rutas de la api

--- routes/api.php
+++ routes/api.php
@@ -19,31 +19,33 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 
 Route::get('/asistenciasSocios','AsistenciaController@getRecords');
+Route::get('/asistenciasInstructores/getRecords','AsistenciasInstructoresController@getRecords');
 
 Route::get('/pagosSocios','SociosMembresiasController@getRecords');
 Route::get('/getTurnoActual','AppStatusController@getTurnoActual');
 Route::put('/updateTurnoActual/{turno}','AppStatusController@updateTurnoActual');
 Route::get('/ingresos/getRecords/{turno}/{fecha?}','IngresosController@getRecords');
 Route::get('/ingresos/getTotal/{fecha?}','IngresosController@getTotal');
-Route::get('/egresos/getTotal/{fecha?}','EgresosController@getTotal');
-Route::get('/egresos/getRecords/{turno}/{fecha?}','EgresosController@getRecords');
+Route::get('/transacciones/getTotal/{fecha?}','TransaccionesController@getTotal');
+Route::get('/transacciones/getRecords/{turno?}/{fecha?}/{tipo?}','TransaccionesController@getRecords');
 Route::post('/pagosSocios','PagosController@storeRecord');
 
 Route::post('productos/addRecord','ProductosController@addRecord');
 
 Route::post('/socio','SociosController@storeRecord');
 Route::put('/socio/{id}','SociosController@updateRecord');
+Route::post('/ventasProductos','VentasController@storeRecord');
 
 Route::get('/compruebaRenovaciones','SociosMembresiasController@getRenovaciones');
 Route::get('/sociosMembresias','SociosMembresiasController@getRecords');
 Route::get('/socioMembresias/{id}','SociosMembresiasController@getRecord');
 Route::get('/sociosyvisitantes','SociosMembresiasController@getSociosyVisitantes');
 Route::get('/socioMembresia/{id}','SociosMembresiasController@getSocioMembresiaById');
+Route::get('/socioMembresia/hasMembresiaActiva/{id}','SociosMembresiasController@hasMembresiaActiva');
 Route::put('/socioMembresia/{id}','SociosMembresiasController@updateRecord');
 
 Route::get('/ventasProductos','VentasController@getRecords');
 Route::delete('/ventasProductos/{id}','VentasController@deleteRecord');
-Route::post('/ventasProductos','VentasController@storeRecord');
 
 Route::get('/pagosSocios','PagosController@getRecords');
 Route::post('/pagosSocios','PagosController@storeRecord');
@@ -53,6 +55,7 @@ Route::get('/visitantesVisitas','VisitantesVisitasController@getRecords');
 Route::post('/visitantesVisitas','VisitantesVisitasController@storeRecord');
 
 Route::resource('asistencias','AsistenciaController');
+Route::resource('asistenciasInstructores','AsistenciasInstructoresController');
 Route::resource('membresias','MembresiasController');
 Route::resource('pagos','PagosController');
 Route::resource('productos','ProductosController');
@@ -61,5 +64,8 @@ Route::resource('statusSocios','StatusSociosController');
 Route::resource('tiposMembresias','TiposMembresiasController');
 Route::resource('ventas','VentasController');
 Route::resource('ingresos','IngresosController');
+Route::resource('instructores','InstructoresController');
+Route::resource('transacciones','TransaccionesController');
 Route::resource('egresos','EgresosController');
+Route::resource('appStatus','AppStatusController');
 
